2f6df837e8c4d17672dba28913c01962c7aefed7
Hassan-Abbas9 <habbas941@gmail.com>
2023-12-02 16:52:58 +0300

    added sum of digits

--- Practice-Recursion-for-final.py
+++ Practice-Recursion-for-final.py
@@ -12,3 +12,22 @@ def factorial(num):
 
 
 print(factorial(5))
+
+
+# Problem 2: Sum of Digits
+#
+# Write a recursive function to find the
+# sum of the digits of a positive integer n.
+
+def sum_of_digits(digits):
+        if digits == 0: # when we get digits // 10 = 0 we just return 0
+            return digits # the base case here is this
+        else:
+
+            return digits % 10 + sum_of_digits(digits // 10)
+            # so an example here is digits % 10 means remainder is THE
+            # last digit here so it's 3 + (remainder digits // 10 which is
+            # 12 then we get 3 + 2 + 1.
+
+print(sum_of_digits(123))
+
