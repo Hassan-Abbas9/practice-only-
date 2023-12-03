6d1a5266953b3ec7b5fc7c16093f72646ef6576f
Hassan-Abbas9 <habbas941@gmail.com>
2023-12-03 18:37:04 +0300

    Added a method to delete the node at the beginning of the list

--- Practice-for-Linked-list.py
+++ Practice-for-Linked-list.py
@@ -28,7 +28,7 @@ class LinkedList:
         self.length += 1
         return True
 
-    def add_node(self, value):
+    def add_node_at_beginning(self, value):
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
@@ -42,8 +42,20 @@ class LinkedList:
             self.head = new_node
             self.head.next = temp
 
-
         self.length += 1
+        return True
+
+    def delete_node_at_beginning(self):
+        delete = self.head
+        if self.length == 0:
+            print("You have no nodes in your linked list")
+        elif self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+
+        self.length -= 1
         return True
 
 
@@ -51,8 +63,10 @@ LL_instance = LinkedList()
 LL_instance.append(30)
 LL_instance.append(20)
 LL_instance.append(10)
-LL_instance.add_node(40)
-LL_instance.add_node(50)
-LL_instance.add_node(60)
+LL_instance.add_node_at_beginning(40)
+LL_instance.add_node_at_beginning(50)
+LL_instance.add_node_at_beginning(60)
+LL_instance.delete_node_at_beginning()
+LL_instance.delete_node_at_beginning()
 LL_instance.display_nodes()
 
