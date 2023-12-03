2ce386529edb2c37d12d3bac7db50e2951c253de
Hassan-Abbas9 <habbas941@gmail.com>
2023-12-03 17:16:36 +0300

    Added display Node and add nodes methods

---
+++ Practice-for-Linked-list.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def display_nodes(self):
+        current = self.head
+        while current:
+            print(current.value, end="--->")
+            current = current.next
+
+    def add_node(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+        return True
+
+
+LL_instance = LinkedList()
+LL_instance.add_node(30)
+LL_instance.add_node(20)
+LL_instance.add_node(10)
+LL_instance.display_nodes()
+
+
+
+
+
+
+
+
