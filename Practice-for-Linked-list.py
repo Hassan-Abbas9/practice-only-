61dafe682737df47372c9fe18c192880514eb28c
Hassan-Abbas9 <habbas941@gmail.com>
2023-12-03 18:14:06 +0300

    added a method to add nodes to the beginning of the LinkedList

--- Practice-for-Linked-list.py
+++ Practice-for-Linked-list.py
@@ -28,17 +28,31 @@ class LinkedList:
         self.length += 1
         return True
 
+    def add_node(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.head.next = None
+        elif self.length == 1:
+            self.head = new_node
+            self.head.next = self.tail
+        else:
+            temp = self.head
+            self.head = new_node
+            self.head.next = temp
+
+
+        self.length += 1
+        return True
+
 
 LL_instance = LinkedList()
 LL_instance.append(30)
 LL_instance.append(20)
 LL_instance.append(10)
+LL_instance.add_node(40)
+LL_instance.add_node(50)
+LL_instance.add_node(60)
 LL_instance.display_nodes()
-
-
-
-
-
-
-
 
