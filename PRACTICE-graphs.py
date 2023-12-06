1ba7ace4e87046b87b8b85da999b5030231f57ee
Hassan-Abbas9 <habbas941@gmail.com>
2023-12-06 23:03:24 +0300

    Added Adjacency List

--- PRACTICE-graphs.py
+++ PRACTICE-graphs.py
@@ -53,11 +53,77 @@ def main():
     graph.display_graph()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
 
 #####################
 #        AL
 #####################
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def add_node(self, node):
+        new_node = Node(node)
+        self.head = new_node
+        self.length += 1
+
+    def display_node(self):
+        temp = self.head
+        while temp:
+            print(temp.value, end='-->')
+            temp = temp.next
+        print("None")
+
+class Graph2:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list:
+            self.adj_list[vertex] = LinkedList()
+            return
+        print(f'vertex {vertex} already exists')
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list and v2 in self.adj_list:
+            self.adj_list[v1].add_node(v1)
+            self.adj_list[v2].add_node(v2)
+        else:
+            print(f"Invalid vertices {v1} and {v2}")
+
+    def display_graph(self):
+        if self.adj_list == {}:
+            print("Graph is empty!\n")
+            return
+        for vertex in self.adj_list:
+            print(vertex + ":", end=" ")
+            self.adj_list[vertex].display_node()
+
+
+if __name__ == '__main__':
+    print("\n#######################")
+    print("          AM           ")
+    print("#######################\n")
+
+    graph = Graph2()
+    graph.display_graph()
+    graph.add_vertex("A")
+    graph.add_vertex("B")
+    graph.add_vertex("C")
+    graph.add_vertex("D")
+    graph.add_vertex("E")
+    graph.add_vertex("C")
+    graph.add_vertex("Z")
+    graph.display_graph()
+    graph.add_edge("A", "B")
+    graph.add_edge("B", "C")
+    graph.add_edge("C", "A")
+    graph.display_graph()
